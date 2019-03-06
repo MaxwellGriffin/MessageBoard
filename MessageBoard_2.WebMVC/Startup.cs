b8d434f5c94140732aafe88757f4db0a2075acff
Max Griffin <maxwell.griffin@outlook.com>
2019-03-06 10:12:14 -0500

    Add project files.

---
+++ MessageBoard_2.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageBoard_2.WebMVC.Startup))]
+namespace MessageBoard_2.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
