ffd6e9bfbc3f7a0f26ee403d97a210bec28febef
Max Griffin <maxwell.griffin@outlook.com>
2019-03-10 16:21:40 -0400

    added post index

---
+++ MessageBoard_2.WebMVC/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using MessageBoard_2.Models.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageBoard_2.WebMVC.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+			var model = new PostListItem[0];
+			return View(model);
+		}
+    }
+}
